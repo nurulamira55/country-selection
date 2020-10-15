9bd886fe91e59ba6ff45c1458e56fe20356a6b2c
nurulamira55 <nurulamira748@ymail.com>
2020-10-15 17:52:33 +0800

    add country page

---
+++ src/views/Country.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+    <b-row class="d-flex justify-content-center">
+        <b-col cols="3">
+            <b-form-select v-model="selectedCountry" :options="countries" value-field="alpha3Code" text-field="name">
+            </b-form-select>
+        </b-col>
+    </b-row>
+
+    <b-row class="mt-5 d-flex justify-content-center" v-if="selectedCountry === 'MYR'">
+        {{selectedCountry}}
+    </b-row>
+
+    <b-row class="mt-5 d-flex justify-content-center">
+        {{selectedCountry}}
+    </b-row>
+
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            countries: [],
+            selectedCountry: '',
+        }
+    },
+
+    created() {
+        this.loadAllCountries()
+
+    },
+
+    methods: {
+
+        loadAllCountries() {
+            axios.get('https://restcountries.eu/rest/v2/all')
+                .then(response => {
+                    this.countries = response.data
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+
+        }
+
+    },
+
+}
+</script>
+
+<style scoped>
+
+</style>
